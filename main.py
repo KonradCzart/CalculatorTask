61da3e57468c7707f81db6e634de506a5dacd49e
Konrad Czart <konradczart@interia.pl>
2021-04-08 18:57:45 +0200

    Add mechanism to show menu in script

--- main.py
+++ main.py
@@ -7,7 +7,16 @@ def div(a, b):
     return a / b
 
 
+def show_menu():
+    print("Wybierz opcje: ")
+    print("1: Dodawanie")
+    print("2: Odejmowanie")
+    print("3: Dzielenie")
+    print("4: Mnozenie")
+    print("5: Wyjscie")
+
+
 if __name__ == '__main__':
-    print("Hello world")
+    show_menu()
 
 
